1086adaf2414cf4f599aa22b02866215df0f44b3
augustocosta79 <amcneto@hotmail.com>
2024-02-06 16:32:18 -0300

    implement savePayable in FileRepository

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import Transaction from "./entities/transaction";
 import TransactionService from "./service/service";
-import MemoryRepository from "./repository/memory";
+import FileRepository from "./repository/file";
 import { cardType } from "./entities/transaction";
 import Card from "./entities/card";
 
 const tx = new Transaction(450, 'Camisa do Flamengo', cardType.credit, new Card('12345678', 'Augusto', '04/29', '362'))
 
-const repository = new MemoryRepository()
+const repository = new FileRepository()
 
 const transactionService = new TransactionService(repository)
 
--- src/repository/file.ts
+++ src/repository/file.ts
@@ -1,13 +1,15 @@
+import Payable from "../entities/payable";
 import Transaction from "../entities/transaction";
 import MemoryRepository from "./memory";
 import fs from "fs";
 
 export default class FileRepository extends MemoryRepository {
   transactionsFile = "transactions.txt";
+  payablesFile = "payables.txt";
 
-  getFileData() {
-    if (fs.existsSync(this.transactionsFile)) {
-      const data = fs.readFileSync(this.transactionsFile).toString();
+  getFileData(fileName: string) {
+    if (fs.existsSync(fileName)) {
+      const data = fs.readFileSync(fileName).toString();
       return JSON.parse(data);
     }
     return [];
@@ -15,18 +17,22 @@ export default class FileRepository extends MemoryRepository {
 
   saveTransaction(tx: Transaction): void {
     let transactions: Transaction[];
-    transactions = this.getFileData();
+    transactions = this.getFileData(this.transactionsFile);
     transactions.push(tx);
     fs.writeFileSync(this.transactionsFile, JSON.stringify(transactions));
   }
 
   getTransactions(): Transaction[] {
     let transactions: Transaction[];
-    transactions = this.getFileData();
+    transactions = this.getFileData(this.transactionsFile);
     return transactions;
   }
 
-  savePayable(): void {
-    
+  savePayable(payable: Payable): void {
+    let payables: Payable[];
+    payables = this.getFileData(this.payablesFile);
+    payables.push(payable);
+    fs.writeFileSync(this.transactionsFile, JSON.stringify(payables));
+    console.log(payables);
   }
 }
