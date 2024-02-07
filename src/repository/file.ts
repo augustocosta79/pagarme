bb11cb029aa17c13946036fcd8e7ba5288f77c98
augustocosta79 <amcneto@hotmail.com>
2024-02-06 23:57:38 -0300

    correct FileRepository implementation

--- src/repository/file.ts
+++ src/repository/file.ts
@@ -1,9 +1,9 @@
 import Payable from "../entities/payable";
 import Transaction from "../entities/transaction";
-import MemoryRepository from "./memory";
+import { TransactionRepository } from "./respository";
 import fs from "fs";
 
-export default class FileRepository extends MemoryRepository {
+export default class FileRepository implements TransactionRepository {
   transactionsFile = "transactions.txt";
   payablesFile = "payables.txt";
 
